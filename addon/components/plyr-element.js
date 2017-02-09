5403068fee837f05d7ef94abcd632d924e81612b
Oskar Rough <oskar@rough.dk>
2017-02-09 14:56:08 +0100

    Fix events not triggering after update

--- addon/components/plyr-element.js
+++ addon/components/plyr-element.js
@@ -1,92 +1,81 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // Formats
-  mp4:        "",
-  webm:       "",
-  captions:   "",
-  poster:     "",
-  mp3:        "",
-  ogg:        "",
-  video_id:   "",
-  options:    null,
-  plyrObject: null,
-  evented:    true,
+  options: null,
+  plyrInstance: null,
+  evented: true,
+
+  // formats
+  mp4: '',
+  webm: '',
+  captions: '',
+  poster: '',
+  mp3: '',
+  ogg: '',
+  video_id: '',
+
   didInsertElement() {
     this._super(...arguments);
-    // this.$().attr('contenteditable', true);
-    const options = this.get('options');
-    if (options !== null) {
-      plyr.setup(this, options);
-    } else {
-      plyr.setup(this);
+    const instance = this.setupPlyr();
+    if (this.get('evented') && instance) {
+      // Bind events.
+      this.get('plyrEvents').forEach(eventName => {
+        instance.on(eventName, e => {
+          this.sendAction(eventName, e);
+        });
+      })
     }
-    // plyr.setup();
-    this.set('plyrObject', this.element.plyr);
+    this.set('plyrInstance', instance);
+  },
 
-    if (this.get('evented') && this.get('plyrObject')) {
-      const media = this.get('plyrObject').media;
-
-      this.$(media).on('abort', (e) => {
-        this.sendAction('abort', e);
-      }).on('loadstart', (e) => {
-        this.sendAction('loadstart', e);
-      }).on('progess', (e) => {
-        this.sendAction('progress', e);
-      }).on('suspend', (e) => {
-        this.sendAction('suspend', e);
-      }).on('error', (e) => {
-        this.sendAction('error', e);
-      }).on('emptied', (e) => {
-        this.sendAction('emptied', e);
-      }).on('stalled', (e) => {
-        this.sendAction('stalled', e);
-      }).on('loadedmetadata', (e) => {
-        this.sendAction('loadedmetadata', e);
-      }).on('loadeddata', (e) => {
-        this.sendAction('loadeddata', e);
-      }).on('canplay', (e) => {
-        this.sendAction('canplay', e);
-      }).on('canplaythrough', (e) => {
-        this.sendAction('canplaythrough', e);
-      }).on('playing', (e) => {
-        this.sendAction('playing', e);
-      }).on('waiting', (e) => {
-        this.sendAction('waiting', e);
-      }).on('seeking', (e) => {
-        this.sendAction('seeking', e);
-      }).on('seeked', (e) => {
-        this.sendAction('seeked', e);
-      }).on('ended', (e) => {
-        this.sendAction('ended', e);
-      }).on('durationchange', (e) => {
-        this.sendAction('durationchange', e);
-      }).on('timeupdate', (e) => {
-        this.sendAction('timeupdate', e);
-      }).on('play', (e) => {
-        this.sendAction('play', e);
-      }).on('pause', (e) => {
-        this.sendAction('pause', e);
-      }).on('ratechange', (e) => {
-        this.sendAction('ratechange', e);
-      }).on('resize', (e) => {
-        this.sendAction('resize', e);
-      }).on('volumechange', (e) => {
-        this.sendAction('volumechange', e);
-      });
+  willDestroyElement() {
+    this._super(...arguments);
+    const instance = this.get('plyrInstance');
+    if (instance) {
+      // Destroying also removes all events.
+      instance.destroy();
     }
   },
-  willDestroyElement() {
-    const plyrObject = this.get('plyrObject');
-    if (plyrObject) {
-      const media = plyrObject.media;
 
-      this.$(media).off('abort loadstart progress suspend error emptied stalled ' +
-        'loadedmetadata loadeddata canplay canplaythrough playing waiting ' +
-        'seeking seeked ended durationchange timeupdate play pause ' +
-        'ratechange resize volumechange');
-
-      plyrObject.destroy();
+  // Sets up a plyr instance on the component's element
+  // and returns the instance.
+  setupPlyr() {
+    const options = this.get('options');
+    let instances;
+    if (options !== null) {
+      instances = plyr.setup(this.element, options);
+    } else {
+      instances = plyr.setup(this.element);
     }
-  }
+    return instances[0]
+  },
+
+  // https://github.com/Selz/plyr#events
+  plyrEvents: [
+    'setup',
+    'ready',
+    'canplay',
+    'canplaythrough',
+    'emptied',
+    'ended',
+    'error',
+    'loadeddata',
+    'loadedmetadata',
+    'loadstart',
+    'pause',
+    'play',
+    'playing',
+    'progress',
+    'seeked',
+    'seeking',
+    'stalled',
+    'timeupdate',
+    'volumechange',
+    'waiting',
+    'enterfullscreen',
+    'exitfullscreen',
+    'captionsenabled',
+    'captionsdisabled',
+    'destroyed'
+  ]
 });
